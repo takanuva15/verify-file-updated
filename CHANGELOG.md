67dfe0f58c5f7ab819a9b1e54c19b6395fb70f3b
takanuva15 <6986426+takanuva15@users.noreply.github.com>
2026-04-10 17:15:10 -0400

    #8 Check all pages of PR files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.2.2] - 2026-04-13
+### Fixed
+- Use paged requests to check all files for PRs with a large number of files changed
+
 ## [1.2.1] - 2022-03-02
 ### Changed
 - Updated this action to be more generic since it can apply to any file in a repo
--- verify_file_updated.sh
+++ verify_file_updated.sh
@@ -5,15 +5,31 @@ echoerr() { printf "%s\n" "$*" >&2; }
 
 #credit to https://stackoverflow.com/a/61922402 for providing a starting point for this script
 
-# Fetch all changed files in this PR and verify the user-specified file is among the changes
-pr_files_json=$(curl -s -u "$OWNER":"$GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "$PR_API_URL/files")
-pr_files=$(jq -r '.[] | .filename' <<<"$pr_files_json")
+# Fetch all changed files in this PR (including paged responses) and verify
+# the user-specified file is among the changes.
+page=1
+per_page=100
 
-# shellcheck disable=SC2199
-if [[ ! ${pr_files[@]} =~ ${FILENAME_TO_CHECK} ]]; then
-  echoerr "Could not find '${FILENAME_TO_CHECK}' among the changed files in this PR! '${FILENAME_TO_CHECK}' must " \
-    "be updated within the PR to pass this check."
-  exit 1
-fi
+while true; do
+  pr_files_json=$(curl -s -u "$OWNER":"$GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+    "$PR_API_URL/files?per_page=$per_page&page=$page")
 
-echo "Detected '${FILENAME_TO_CHECK}' among the changed files in this PR. Verification succeeded!"
+  page_files=$(jq -r '.[] | .filename' <<<"$pr_files_json")
+
+  # Stop when the API returns no files for the next page.
+  if [[ -z "$page_files" ]]; then
+    break
+  fi
+
+  # Return early as soon as the required file is found on any page.
+  if grep -Fxq "$FILENAME_TO_CHECK" <<<"$page_files"; then
+    echo "Detected '${FILENAME_TO_CHECK}' among the changed files in this PR. Verification succeeded!"
+    exit 0
+  fi
+
+  page=$((page + 1))
+done
+
+echoerr "Could not find '${FILENAME_TO_CHECK}' among the changed files in this PR! '${FILENAME_TO_CHECK}' must " \
+  "be updated within the PR to pass this check."
+exit 1
